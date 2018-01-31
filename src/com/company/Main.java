1050d0821f187392f2037e4a985d0e27f855fd7c
Tlenc <tlenciauskas@gmail.com>
2018-01-31 14:10:30 +0200

    Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static final int EXIT_CODE = 0; /* konstanta */
     public static void main(String[] args) {
-	// write your code here
+        Main objektas = new Main();
+    }
+
+    public Main() {
+        float suma = 0;
+        float sk = 0;
+        do {
+            sk = duokGarantuotaiSk();
+            suma += sk;
+        } while (sk != EXIT_CODE);
+        System.out.println("Suma : " + suma);
+    }
+/* try catch */
+    public float duokGarantuotaiSk() {
+        float rezultatas = 0;
+        Scanner sc = new Scanner(System.in);
+        boolean arPavyko = false;
+        while (arPavyko == false) {
+            System.out.println("Įveskite skaičių");
+            try {
+                rezultatas = sc.nextFloat();
+                arPavyko = true;
+            } catch (Exception klaida) {
+                System.out.println("Įvestas tekstas yra ne skaičius");
+                sc.next();
+            }
+        }
+        return rezultatas;
     }
 }
